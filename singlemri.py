d4ca36faffacf75fc2809fee8d785f481b4e1e8a
Muhammad Usman Akbar <45659899+muhamadusman@users.noreply.github.com>
2024-04-16 00:26:13 +0500

    Update singlemri.py

--- singlemri.py
+++ singlemri.py
@@ -1,36 +1,3 @@
-# import os
-# import subprocess
-# import shutil
-# import argparse
-
-# def get_file_type(dicom_file):
-#     output = subprocess.check_output(["/app/dicom_hdr", dicom_file])
-#     return output.decode("utf-8")
-
-# def main(input_folder, output_folder):
-#     # Create the output folder if it doesn't exist
-#     if not os.path.exists(output_folder):
-#         os.makedirs(output_folder)
-
-#     # Loop over DICOM files in the input folder
-#     for filename in os.listdir(input_folder):
-#         if filename.endswith(".dcm"):
-#             dicom_file = os.path.join(input_folder, filename)
-#             file_type = get_file_type(dicom_file)
-#             print(file_type)
-#             if "Sag 3D T1 GD" in file_type:
-#                 shutil.copy(dicom_file, output_folder)
-#                 print("Copied", dicom_file, "to", output_folder)
-#             elif "Sagadsdsad" in file_type:
-#                 pass  # You can add additional logic here for other file types
-
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser(description="Sort DICOM files by sequence type.")
-#     parser.add_argument("input_folder", help="Path to the input DICOM folder.")
-#     parser.add_argument("output_folder", help="Path to the output folder for the sorted DICOM files.")
-#     args = parser.parse_args()
-
-#     main(args.input_folder, args.output_folder)
 
 import os
 import shutil
@@ -42,7 +9,6 @@ def get_file_type(dicom_file):
     ds = pydicom.dcmread(dicom_file)
     
     # Example: Retrieve Modality and Series Description as file type
-    # Feel free to adjust this based on your specific criteria for sorting
     file_type = ds.get("Modality", "") + " " + ds.get("SeriesDescription", "")
     return file_type
 
@@ -57,10 +23,10 @@ def main(input_folder, output_folder):
             dicom_file = os.path.join(input_folder, filename)
             try:
                 file_type = get_file_type(dicom_file)
-                print(file_type)
+                # print(file_type)
                 if "Sag 3D T1 GD" in file_type:
                     shutil.copy(dicom_file, output_folder)
-                    print(f"Copied {dicom_file} to {output_folder}")
+                    # print(f"Copied {dicom_file} to {output_folder}")
                 elif "Sagadsdsad" in file_type:
                     pass  # You can add additional logic here for other file types
             except Exception as e:
@@ -73,3 +39,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args.input_folder, args.output_folder)
+    
