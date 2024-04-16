4e96520e3d6886f1ce013d7f3921dc03068e492d
Muhammad Usman Akbar <45659899+muhamadusman@users.noreply.github.com>
2024-04-16 12:02:35 +0500

    Update getnifty.py

--- getnifty.py
+++ getnifty.py
@@ -1,135 +1,184 @@
 import os
-import torch
-import numpy as np
+import shutil
 import nibabel as nib
 import scipy.ndimage
-from monai.transforms import Compose, LoadImaged, EnsureChannelFirstd, NormalizeIntensityd, EnsureTyped
-from monai.networks.nets import SegResNet
-from monai.inferers import sliding_window_inference
-import collections
-import argparse
 import glob
+import sys
+import argparse
+import numpy as np
+from scipy.ndimage import zoom
 
-def get_transform():
-    return Compose([
-        LoadImaged(keys=["image"]),
-        EnsureChannelFirstd(keys=["image"]),
-        NormalizeIntensityd(keys=["image"], nonzero=True, channel_wise=True),
-        EnsureTyped(keys=["image"]),
-    ])
+def padAroundImageCenter(imageArray, paddedSize):
+    origShape = imageArray.shape
+    diff = (np.array(paddedSize) - np.array(origShape)) / 2
+    extraPadding = [(int(np.floor(d)), int(np.ceil(d))) for d in diff]
+    paddedImageArray = np.pad(imageArray, extraPadding, mode='constant', constant_values=0)
+    return paddedImageArray
 
-def load_model_from_npz(model_path, device):
-    model = SegResNet(
-        blocks_down=[1, 2, 2, 4],
-        blocks_up=[1, 1, 1],
-        init_filters=16,
-        in_channels=4,
-        out_channels=4,
-        dropout_prob=0.2,
-    ).to(device)
+def flip_volume_if_needed(nifti_file, data):
+    specific_text = "Ax_T2_GD"
+    filename = os.path.basename(nifti_file)
+    
+    if specific_text in filename:
+        print(f"Flipping {filename} vertically before resampling.")
+        data = np.flip(data, axis=0)  # Flipping operation
+        
+    return data
 
-    # Load .npz file containing the weights
-    npz_weights = np.load(model_path, allow_pickle=True)
-    model_state_dict = model.state_dict()
 
-    new_state_dict = collections.OrderedDict()
-    for i, (key, value) in enumerate(model_state_dict.items()):
-        layer_weights = torch.from_numpy(npz_weights[str(i)])
-        new_state_dict[key] = layer_weights
-
-    model.load_state_dict(new_state_dict)
-    return model
-
-def upsample_and_reorient_volume(nifti_file, original_shape, original_spacing, reference_volume_path, output_folder):
+def resample_and_reshape_volume(nifti_file, new_shape, new_spacing, output_folder):
+    
     img = nib.load(nifti_file)
     data = img.get_fdata()
     affine = img.affine
 
-    resize_factor = np.array(original_shape) / np.array(data.shape)
-    resampled_data = scipy.ndimage.zoom(data, resize_factor, order=0)
+    # Debugging print statement
+    print(f"Processing file: {os.path.basename(nifti_file)}")
 
-    if resampled_data.shape != original_shape:
-        additional_resizing_factor = np.array(original_shape) / np.array(resampled_data.shape)
-        resampled_data = scipy.ndimage.zoom(resampled_data, additional_resizing_factor, order=0)
+    specific_text = "Ax_T2_GD"
+    filename = os.path.basename(nifti_file)
+    
+    if specific_text in filename:
+        print(f"Flipping {filename} vertically before resampling.")
+        data = np.flip(data, axis=0)  # Flipping operation
+        
+    
+    current_spacing = np.array(img.header.get_zooms()[:3])
+    resize_factor = current_spacing / new_spacing
+    new_real_shape = data.shape * resize_factor
+    resampled_data = scipy.ndimage.zoom(data, resize_factor, order=2)
+
+    resampled_data = padAroundImageCenter(resampled_data, new_shape)
 
     new_affine = np.copy(affine)
-    np.fill_diagonal(new_affine, np.concatenate((original_spacing, [affine[3,3]])))
+    np.fill_diagonal(new_affine, list(new_spacing) + [1])
+    new_img = nib.Nifti1Image(resampled_data, new_affine)
+    output_path = os.path.join(output_folder, os.path.basename(nifti_file))
+    nib.save(new_img, output_path)
 
-    upsampled_img = nib.Nifti1Image(resampled_data, new_affine)
-    upsampled_img.header.set_zooms(original_spacing)
+def reorient_to_standard(input_volume_path):
+    # Load the original volume data
+    volume_nii = nib.load(input_volume_path)
 
-    reference_img = nib.load(reference_volume_path)
-    first_3d_volume_data = reference_img.dataobj[..., 0]
-    first_3d_volume_img = nib.Nifti1Image(first_3d_volume_data, reference_img.affine, header=reference_img.header)
+    # Reorient the volume to RAS+ orientation (Right, Anterior, Superior)
+    volume_nii = nib.as_closest_canonical(volume_nii)
 
-    reoriented_img = nib.Nifti1Image(upsampled_img.get_fdata(), first_3d_volume_img.affine, header=upsampled_img.header)
+    return volume_nii
 
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    output_path = os.path.join(output_folder, os.path.basename(nifti_file).replace('.nii', '_corrected.nii'))
-    nib.save(reoriented_img, output_path)
-    print(f"Processed volume saved to {output_path}")
+def process_volumes_in_folder(input_folder, output_folder, new_shape, new_spacing):
+    nifti_files = [f for f in glob.glob(os.path.join(input_folder, '*.nii'))]
+    if not nifti_files:
+        print(f"No NIfTI files found in {input_folder}. Exiting.")
+        sys.exit(1)
 
-def predict_and_process_volume(input_volume_path, model, device, transform, original_shape, original_spacing, reference_volume_path, output_folder):
-    data = {"image": input_volume_path}
-    data = transform(data)
-    img = data["image"].unsqueeze(0).to(device)
+    for nifti_file in nifti_files:
+        resample_and_reshape_volume(nifti_file, new_shape, new_spacing, output_folder)
 
-    model.eval()
-    with torch.no_grad():
-        output = sliding_window_inference(img, (256, 256, 120), 4, model, overlap=0.5)
-    segmentation_mask = torch.argmax(output, dim=1).squeeze().cpu().numpy()
 
-    tmp_output_path = os.path.join(output_folder, "tmp_segmentation.nii")
-    print ("Final Segmentation mask saved : ", tmp_output_path)
-    nib.save(nib.Nifti1Image(segmentation_mask.astype(np.uint8), np.eye(4)), tmp_output_path)
 
-    upsample_and_reorient_volume(tmp_output_path, original_shape, original_spacing, reference_volume_path, output_folder)
-    os.remove(tmp_output_path)
-  
+def resample_volume(nifti_file, output_folder, target_shape):
+    img = nib.load(nifti_file)
+    data = img.get_fdata()
+    
+    # Calculate new zooms to get to the target shape
+    current_shape = np.array(data.shape)
+    target_shape = np.array(target_shape)
+    new_zooms = target_shape / current_shape
+    
+    # Use scipy's zoom function to resample the volume data
+    resampled_data = zoom(data, new_zooms, order=3)  # Cubic interpolation
+    
+    # Prepare the new NIfTI image
+    new_affine = np.copy(img.affine)
+    # Adjust the affine matrix to account for the new zooms
+    scaling_affine = np.diag(new_zooms.tolist() + [1])
+    new_affine = np.dot(new_affine, np.linalg.inv(scaling_affine))
+    
+    new_img = nib.Nifti1Image(resampled_data, new_affine)
+    
+    # Save the new volume
+    output_path = os.path.join(output_folder, os.path.basename(nifti_file))
+    nib.save(new_img, output_path)
 
-def find_nii_volume_with_name(directory, pattern):
-    # Construct the search pattern
-    search_pattern = os.path.join(directory, f"*{pattern}*.nii")
+def process_volumes_in_folder_0(input_folder, output_folder, target_shape, text_fragments):
+    nifti_files = glob.glob(os.path.join(input_folder, '*.nii'))
+    
+    if not nifti_files:
+        print(f"No NIfTI files found in {input_folder}. Exiting.")
+        return
+    
+    for nifti_file in nifti_files:
+        # Check if the file name contains any of the specified substrings
+        if any(fragment in os.path.basename(nifti_file) for fragment in text_fragments):
+            resample_volume(nifti_file, output_folder, target_shape)
+            print(f"Processed {os.path.basename(nifti_file)}")
+        else:
+            print(f"Skipped {os.path.basename(nifti_file)} due to name mismatch")
 
-    # Use glob to find files matching the pattern
-    matching_files = glob.glob(search_pattern)
 
-    # Check if we found any matching files
-    if matching_files:
-        # For simplicity, return the first matching file's full path
-        return matching_files[0]
-    else:
-        return None
+
+
+def concatenate_nifti_volumes(input_folder, output_path):
+    nifti_files = reversed(sorted(glob.glob(os.path.join(input_folder, '*.nii'))))
+    if not nifti_files:
+        print(f"No NIfTI files found in {input_folder} for concatenation. Exiting.")
+        sys.exit(1)
+
+    # volume_to_flip = "Dicom_Ax_T2_GD_a_20000101000101_6.nii"
+    concatenated_data = []
+
+    for nifti_file in nifti_files:
+        img = nib.load(nifti_file)
+        img_data = img.get_fdata()
+        # if os.path.basename(nifti_file) == volume_to_flip:
+            # img_data = np.flip(img_data, axis=0)
+        concatenated_data.append(img_data)
+
+    concatenated_data = np.stack(concatenated_data, axis=-1)
+    concatenated_img = nib.Nifti1Image(concatenated_data, img.affine)
+    nib.save(concatenated_img, output_path)
+    print(f"Saved concatenated volume to {output_path}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run inference on a NIfTI volume.")
-    parser.add_argument("--input_volume_path", required=True, help="Path to the input NIfTI volume.")
-    parser.add_argument("--output_folder", required=True, help="Directory to save the output.")
-    
+    parser = argparse.ArgumentParser(description='Process NIfTI volumes.')
+    parser.add_argument('--source_dir', required=True, help='Directory containing source NIfTI files.')
+    parser.add_argument('--temp_dir', required=True, help='Temporary directory for intermediate processing.')
+    parser.add_argument('--resampled_dir', required=True, help='Directory for resampled NIfTI volumes.')
+    parser.add_argument('--final_dir', required=True, help='Directory to save the final NIfTI volume.')
     args = parser.parse_args()
-    
-    base_path = "/appdata/" 
-    ref_nifty_path = "/data/"
-    input_volume_path = args.input_volume_path
-    output_folder = args.output_folder
-    
-    print ("Input Volume  : ", input_volume_path)
-    print (" Output volume Path : ", output_folder)
-    # input_volume = os.path.join(input_volume_path, "final_nifti_volume.nii")
-    model_path = os.path.join(base_path, "Model_Weights.npz")
-    
-    matching_volume = find_nii_volume_with_name(ref_nifty_path, "Sag_3D_T1_GD")
-    reference_volume_path = os.path.join(ref_nifty_path, matching_volume )#"dicom_Sag_3D_T1_GD_a_20000101000101_7.nii")
 
-    # Ensure the output directory exists
-    os.makedirs(output_folder, exist_ok=True)
+    source_dir = args.source_dir
+    temp_dir = args.temp_dir
+    resampled_dir = args.resampled_dir
+    final_dir = args.final_dir
 
-    original_shape = (392, 512, 512)  # Adjust as per your requirements
-    original_spacing = (0.40000153, 0.4688, 0.4688)  # Adjust as per your requirements
+    text_fragments = ["Sag_3D_T1_GD", "Sag_3D_T2_flair", "Sag_3D_T1", "Ax_T2_GD"]
+    new_shape = (256, 256, 140)
+    new_spacing = (1.0, 1.0, 2.0)
+
+    # Step 1: Copy relevant .nii files to temp directory
+    for filename in os.listdir(source_dir):
+        if filename.endswith('.nii') and any(text in filename for text in text_fragments):
+            try:
+                source_file_path = os.path.join(source_dir, filename)
+                dest_file_path = os.path.join(temp_dir, filename)
+                shutil.copy2(source_file_path, dest_file_path)
+                print(f"Copied: {filename}")
+            except Exception as e:
+                print(f"Failed to copy {filename}: {e}")
+        else:
+            print(f"Skipping file not matching criteria: {filename}")
+
+
+
+    target_shape = (512, 512, 392)  # The desired new shape
+    text_fragments = ["Sag_3D_T1_GD", "Sag_3D_T2_flair", "Sag_3D_T1"]
+
+    process_volumes_in_folder_0(temp_dir, temp_dir, target_shape, text_fragments)
     
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    transform = get_transform()
-    model = load_model_from_npz(model_path, device)
-    predict_and_process_volume(input_volume_path, model, device, transform, original_shape, original_spacing, reference_volume_path, output_folder)
+    # Step 2: Resample and reshape
+    process_volumes_in_folder(temp_dir, resampled_dir, new_shape, new_spacing)
     
+    # Step 3: Concatenate
+    final_output_path = os.path.join(final_dir, 'final_nifti_volume.nii')
+    concatenate_nifti_volumes(resampled_dir, final_output_path)
